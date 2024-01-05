03972799302d2ef7a6362918d36cee245c2819d1
Denis <43709752+Deniszonta@users.noreply.github.com>
2024-01-05 21:56:00 +0100

    create new connection string

--- web/Program.cs
+++ web/Program.cs
@@ -5,7 +5,7 @@ using web.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("TempusContext");
+var connectionString = builder.Configuration.GetConnectionString("AzureContext");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
